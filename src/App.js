a1c9c8f239feabb80cb9922b63990b8f8d189a6b
[aditya gangil] <[adityagangil2001@gmail.com]>
2023-11-16 18:56:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Row, Col, Card, Button, Navbar, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Button, Navbar, Nav, Badge, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const productsArr = [
@@ -29,20 +29,60 @@ const productsArr = [
   },
 ];
 
-const ProductCard = ({ name, title, price, imageUrl }) => (
-  
+const ProductCard = ({ name, title, price, imageUrl, addToCart }) => (
   <Card style={{ width: '18rem', margin: '10px' }}>
     {name}
     <Card.Img variant="top" src={imageUrl} />
     <Card.Body>
       <Card.Title>{title}</Card.Title>
       <Card.Text>${price}</Card.Text>
-      <Button variant="primary">Add to Cart</Button>
+      <Button variant="primary" onClick={() => addToCart({ name, title, price, imageUrl })}>
+        Add to Cart
+      </Button>
     </Card.Body>
   </Card>
 );
 
+// ... (other imports)
+
+const Cart = ({ cartElements, removeFromCart, onHide }) => (
+  <Modal show={true} onHide={onHide}>
+    <Modal.Header closeButton>
+      <Modal.Title>Cart</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      {cartElements.map((cartItem, index) => (
+        <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <img src={cartItem.imageUrl} alt={cartItem.title} style={{ width: '50px', marginRight: '10px' }} />
+          <div style={{ flex: 1 }}>
+            <p>{cartItem.title}</p>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <p>${cartItem.price}</p>
+              <Button variant="danger" onClick={() => removeFromCart(index)}>
+                Remove
+              </Button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </Modal.Body>
+  </Modal>
+);
+
 function App() {
+  const [cartElements, setCartElements] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const addToCart = (product) => {
+    setCartElements([...cartElements, { ...product, quantity: 1 }]);
+  };
+
+  const removeFromCart = (index) => {
+    const updatedCart = [...cartElements];
+    updatedCart.splice(index, 1);
+    setCartElements(updatedCart);
+  };
+
   return (
     <div>
       <Navbar bg="light" expand="lg">
@@ -56,7 +96,15 @@ function App() {
               <Nav.Link href="#about" style={{ fontSize: '30px', marginRight: '50px' }}>About</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#cart" style={{ fontSize: '30px', marginLeft: '40vh' }} className="ml-auto">Cart</Nav.Link>
+              <Nav.Link
+                href="#cart"
+                style={{ fontSize: '30px', marginLeft: '40vh' }}
+                className="ml-auto"
+                onClick={() => setIsCartOpen(true)}
+              >
+                Cart{' '}
+                <Badge variant="secondary">{cartElements.length}</Badge>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -72,12 +120,17 @@ function App() {
         <Row>
           {productsArr.map((product, index) => (
             <Col key={index}>
-              <ProductCard {...product} />
+              <ProductCard {...product} addToCart={addToCart} />
             </Col>
           ))}
         </Row>
       </Container>
 
+      {isCartOpen && (
+        <Container className="mt-4 text-center">
+          <Cart cartElements={cartElements} removeFromCart={removeFromCart} onHide={() => setIsCartOpen(false)} />
+        </Container>
+      )}
     </div>
   );
 }
